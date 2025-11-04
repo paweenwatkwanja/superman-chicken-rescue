a0d78b3574ed2bb0f14ab44ad4eebfc48d271e19
paquesqueue <paquesqueue@gmail.com>
2025-11-04 14:00:59 +0700

    feat: first version with timelapse

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/paweenwatkwanja/superman-chicken-rescue/solution"
 	"github.com/paweenwatkwanja/superman-chicken-rescue/utils"
@@ -14,7 +15,12 @@ func main() {
 		panic(err)
 	}
 
+	start := time.Now()
+
 	var maxCoveredChicken int
 	maxCoveredChicken = solution.Rescue(rescueInput.RoofSize, rescueInput.Positions, maxCoveredChicken)
 	fmt.Println(maxCoveredChicken)
+
+	elapsed := time.Since(start)
+	fmt.Printf("Duration: %v\n", elapsed)
 }
